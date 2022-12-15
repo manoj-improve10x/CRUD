ff5a8cffecada6a7a69ae29d849ebb3cd7c75575
manoj <manoj.mopoori@improve10x.com>
2022-12-15 17:25:03 +0530

    DashboardItemsActivity-change objectNames

--- app/src/main/java/com/example/crud/dashboard/DashboardItemsActivity.java
+++ app/src/main/java/com/example/crud/dashboard/DashboardItemsActivity.java
@@ -14,7 +14,6 @@ public class DashboardItemsActivity extends BaseActivity {
 
     private ArrayList<DashboardItem> dashboardItems;
     private RecyclerView dashboardItemsRv;
-    //Todo: change DashboardAdapter name
     private DashboardItemsAdapter dashboardItemsAdapter;
 
     @Override
@@ -27,27 +26,25 @@ public class DashboardItemsActivity extends BaseActivity {
         setupDashboardItemsRv();
     }
 
-    //ToDo: Implement progressBar in all activities
     private void setupData() {
         dashboardItems = new ArrayList<>();
 
-        //Todo: DashboardItemsActivity-change objectNames
-        DashboardItem dashboardItem1 = new DashboardItem();
-        dashboardItem1.imageUrl = "https://e7.pngegg.com/pngimages/169/794/png-clipart-ios-message-icon-iphone-message-computer-icons-text-messaging-messenger-electronics-grass.png";
-        dashboardItem1.title = "Messages";
-        dashboardItems.add(dashboardItem1);
-        DashboardItem dashboardItem2 = new DashboardItem();
-        dashboardItem2.imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTF24qPNXvYKYx3rZvmPm0bGHRjHqnZqo4xjTMYCdycRaJaEwQWaFixVqNIfjrBC_goSxo&usqp=CAU";
-        dashboardItem2.title = "Templates";
-        dashboardItems.add(dashboardItem2);
-        DashboardItem dashboardItem3 = new DashboardItem();
-        dashboardItem3.imageUrl = "https://www.google.com/search?rlz=1C1JJTC_enIN1022IN1022&sxsrf=ALiCzsZuPMiIe7SloM6SUgGMo5LJTYASog:1670913223759&q=movie+folder+png&tbm=isch&sa=X&ved=2ahUKEwiD-pvp_PX7AhVwH7cAHUFeAEcQ0pQJegQIDBAB&biw=1536&bih=746&dpr=1.25#imgrc=oXmHMYeeUnfyoM";
-        dashboardItem3.title = "Movies";
-        dashboardItems.add(dashboardItem3);
-        DashboardItem dashboardItem4 = new DashboardItem();
-        dashboardItem4.title = "SeriesList";
-        dashboardItem4.imageUrl = "https://www.google.com/search?rlz=1C1JJTC_enIN1022IN1022&sxsrf=ALiCzsZuPMiIe7SloM6SUgGMo5LJTYASog:1670913223759&q=movie+folder+png&tbm=isch&sa=X&ved=2ahUKEwiD-pvp_PX7AhVwH7cAHUFeAEcQ0pQJegQIDBAB&biw=1536&bih=746&dpr=1.25#imgrc=oXmHMYeeUnfyoM";
-        dashboardItems.add(dashboardItem4);
+        DashboardItem messages = new DashboardItem();
+        messages.imageUrl = "https://e7.pngegg.com/pngimages/169/794/png-clipart-ios-message-icon-iphone-message-computer-icons-text-messaging-messenger-electronics-grass.png";
+        messages.title = "Messages";
+        dashboardItems.add(messages);
+        DashboardItem templates = new DashboardItem();
+        templates.imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTF24qPNXvYKYx3rZvmPm0bGHRjHqnZqo4xjTMYCdycRaJaEwQWaFixVqNIfjrBC_goSxo&usqp=CAU";
+        templates.title = "Templates";
+        dashboardItems.add(templates);
+        DashboardItem movies = new DashboardItem();
+        movies.imageUrl = "https://www.google.com/search?rlz=1C1JJTC_enIN1022IN1022&sxsrf=ALiCzsZuPMiIe7SloM6SUgGMo5LJTYASog:1670913223759&q=movie+folder+png&tbm=isch&sa=X&ved=2ahUKEwiD-pvp_PX7AhVwH7cAHUFeAEcQ0pQJegQIDBAB&biw=1536&bih=746&dpr=1.25#imgrc=oXmHMYeeUnfyoM";
+        movies.title = "Movies";
+        dashboardItems.add(movies);
+        DashboardItem seriesItems = new DashboardItem();
+        seriesItems.title = "SeriesList";
+        seriesItems.imageUrl = "https://www.google.com/search?rlz=1C1JJTC_enIN1022IN1022&sxsrf=ALiCzsZuPMiIe7SloM6SUgGMo5LJTYASog:1670913223759&q=movie+folder+png&tbm=isch&sa=X&ved=2ahUKEwiD-pvp_PX7AhVwH7cAHUFeAEcQ0pQJegQIDBAB&biw=1536&bih=746&dpr=1.25#imgrc=oXmHMYeeUnfyoM";
+        dashboardItems.add(seriesItems);
     }
 //Todo: Create new Method for ids in all classes give proper name
 
